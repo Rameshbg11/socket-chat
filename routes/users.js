43db1f70e1605ad59cce50d4742f3c88a143031b
Ramesh <ramesh.g@neutrinos.co>
2019-01-27 19:25:32 +0200

    post and get auth api created

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const config = require('../config/database');
-
+const User = require('../models/user');
+const Group = require('../models/group');
 
 // Register a user
 router.post('/register', (req, res, next) => {
@@ -14,7 +14,15 @@ router.post('/register', (req, res, next) => {
         password: req.body.password,
         email: req.body.email,
     });
-    User.getUserByUsername(req.body.username, (err, user) => {
+    let key;
+
+    const password = req.body.password;
+    if ('username' in req.body)
+        key = 'username';
+    else if ('email' in req.body)
+        key = 'email';
+
+    User.getUserByKey(key, req.body[key], (err, user) => {
         if (err) throw err;
         if (user)
             return res.json({ success: false, msg: 'User already exists' });
@@ -59,10 +67,10 @@ router.post('/authenticate', (req, res, next) => {
             if (isMatch) {
                 const token = jwt.sign(user.toJSON(), config.secret, {
                     expiresIn: 604800 //1 week
-                }); 
+                });
                 res.json({
                     success: true,
-                    token: 'JWT ' + token,                   
+                    token: 'JWT ' + token,
                     expVal: jwt.decode(token).exp,
                     user: {
                         name: user.name,
@@ -80,7 +88,7 @@ router.post('/authenticate', (req, res, next) => {
     });
 });
 
-// Profile
+// Get Profile
 router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
     res.json({ user: req.user });
 });
@@ -109,6 +117,64 @@ function getToken() {
             });
         }
     });
+}
+
+const checkToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+    if (typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+        req.token = token;
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403);
+    }
+}
+
+// Create grp and push chat
+router.post('/msg', checkToken, function (req, res, next) {
+    let statusCode = (verifyToken(req.token, config.secret));
+    if (statusCode === 400) {
+        Group.updateOne({ groupName: req.body.groupName }, { $push: { msgList: [req.body.userMsg] } }, { upsert: true }).then(function (grp) {
+            res.send(grp);
+        }).catch(next);
+        return res.json({ status: 400, msg: 'SUCCESS: Connected to protected route' });
+    }
+    else if (statusCode === 205)
+        return res.json({ status: 215, msg: 'No token provided' });
+    else
+        return res.json({ status: 500, msg: 'Error' });
+});
+
+
+// Get all chats
+router.get('/getallchats', checkToken, function (req, res, next) {
+    let statusCode = (verifyToken(req.token, config.secret));
+    if (statusCode === 400) {
+        Group.find({}).then(function (result) {
+            res.send(result);
+        });
+        // return res.json({ status: 400, msg: 'SUCCESS: Connected to protected route' });
+    }
+    else if (statusCode === 205)
+        return res.json({ status: 215, msg: 'No token provided' });
+    else
+        return res.json({ status: 500, msg: 'Error' });
+});
+
+
+function verifyToken(token, secret) {
+    let status;
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err)
+            status = 215;
+        else if (decoded)
+            status = 400;
+        else
+            status = 500;
+    });
+    return status;
 }
 
 module.exports = router;
